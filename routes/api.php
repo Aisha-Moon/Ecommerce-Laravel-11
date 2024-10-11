fa915eedd2cb20547b5a56f1ca21e1df28ef75f1
Aisha-Moon <aishaaktermoon2@gmail.com>
2024-10-11 12:34:47 +0600

    product crud routes added

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,14 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Backend\Api\BrandController;
+use App\Http\Controllers\Backend\Api\ProductController;
 use App\Http\Controllers\Backend\Api\CategoryController;
 
 Route::apiResource('brands', BrandController::class);
 Route::apiResource('categories', CategoryController::class);
+Route::apiResource('products', ProductController::class);
+
+Route::get('/test', function () {
+     return response()->json(['message' => 'API is working!']);
+ });
+ 
